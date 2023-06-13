e404b86fa7489027ded5c22a867b03889b8ebf5d
Shaq Burgin <burginmesech@gmail.com>
2023-06-13 10:05:09 -0500

    commit

--- hoursofday.js
+++ hoursofday.js
@@ -13,3 +13,16 @@ if (hourNow > 18) {
 }
 
 document.write('<h3>' + greeting + '</h3>');
+
+// Create three variables to store the information needed.
+var price =5;
+var quantity=14;
+var total = price * quantity;
+var el = document.getElementById("cost");
+el.textContent = '$' + today;
+
+
+// Get the element with an id of cost.
+
+
+// Note: textContent does not work in IE8 or earlier - see explanation on website
