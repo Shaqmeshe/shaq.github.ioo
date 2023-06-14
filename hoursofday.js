e72acd5d7324ee4f1dfc3afe0caa304b5565a1db
Shaq Burgin <burginmesech@gmail.com>
2023-06-13 19:18:40 -0500

    commit

--- hoursofday.js
+++ hoursofday.js
@@ -7,3 +7,9 @@ var elStock = document.getElementById('stock');
 elStock.className = inStock;
 var elShip = document.getElementById('ship');
 elShip.className = shipping;
+
+
+var colors; 
+colors = ['white', 'black', 'custom'];
+var el = document.getElementById('my box');
+el.textContent = colors[0];
